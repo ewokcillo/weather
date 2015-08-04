9acd387c53d243bc4b8196b5a46a3859237c2a97
ewokcillo <azotillo@gmail.com>
2015-08-04 17:43:50 +0200

    added loggin

--- api_rest.py
+++ api_rest.py
@@ -2,6 +2,8 @@
 from flask import Flask
 from requests import get
 
+import logging
+from logging import FileHandler
 import json
 
 from settings import *
@@ -22,12 +24,17 @@ def get_weather(city):
             return_data = last_day['temp']
         except:
             return_data = {'error': "Error parsing weather data"}
+            app.logger.error(return_data)
     else:
         return_data = {'error': "Error obtaining data form openweathermap"}
+        app.logger.error(return_data)
 
     return json.dumps(return_data)
 
 
 if __name__ == '__main__':
-    app.debug = True
+    file_handler = FileHandler(LOG_FILE)
+    file_handler.setLevel(LOG_LEVEL)
+    app.logger.addHandler(file_handler)
+    app.debug = DEBUG
     app.run(IP_HOST, PORT_HOST)
--- settings.py
+++ settings.py
@@ -1,8 +1,15 @@
 # coding=utf-8
+import logging
+
 """
 Settings to add the global values in the webservice and the command line tool
 """
+# WebService settings
 IP_HOST='0.0.0.0'
 PORT_HOST=5000
-
+DEBUG=True
 WEATHER_DATA="http://api.openweathermap.org/data/2.5/forecast/daily?q={0}&units=metric&cnt=2"
+
+#logging
+LOG_FILE='logs/api.log'
+LOG_LEVEL=logging.DEBUG
